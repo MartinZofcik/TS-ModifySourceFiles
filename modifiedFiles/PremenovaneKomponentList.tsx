ab4858aa23a155becf7ef75e9f0f45204974e4ba
Martin Zofcik <martinzof1@gmail.com>
2023-08-26 14:53:22 +0200

    Add RenamedComponent to showcase how it works

--- modifiedFiles/PremenovaneKomponentList.tsx
+++
@@ -1,5 +0,0 @@
-export const PremenovaneKomponent = (props) => {
-  [data] = usePremenovaneQuery()
-  const PremenovaneLiteral = "Premenovane string";
-  return data.map(d => <PremenovaneKomponent data={d} />
-}

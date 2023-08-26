8287ffb6e9c53be19cac321d04a255008b5cfd32
Martin Zofcik <martinzof1@gmail.com>
2023-08-26 14:47:46 +0200

    Add working project without input and output file, still hardcoded

--- modify.js
+++ modify.js
@@ -2,44 +2,71 @@ import { Project, SyntaxKind } from "ts-morph";
 
 const project = new Project({ tsConfigFilePath: "tsconfig.json" });
 
-var newVariableDec, newExpression, newFilePath;
+var newVariableDec, newExpression, newFileName, newStringLiteral;
+const renameTo = process.argv.slice(2);
 
-for (const sourceFile of project.getSourceFiles("EntityKomponentList.tsx")) {
-  // rename variables containing "entity" || "Entity"
+// loop over entire folder
+for (const sourceFile of project.getSourceFiles("./toBeModified/*.tsx")) {
+  // rename variables of type SyntaxKind.VariableDeclaration containing "entity" || "Entity"
   for (const variableDec of sourceFile.getDescendantsOfKind(
     SyntaxKind.VariableDeclaration
   )) {
-    if (variableDec.getName().indexOf("Entity") > -1) {
-      newVariableDec = variableDec.getName().replace("Entity", "Premenovane");
+    // if variable contains "Entity", rename
+    if (variableDec.getName().match(/Entity/g) != -1) {
+      newVariableDec = variableDec.getName().replace("Entity", renameTo);
       variableDec.rename(newVariableDec);
     }
-    if (variableDec.getName().indexOf("entity") > -1) {
-      newVariableDec = variableDec.getName().replace("entity", "premenovane");
+    // if variable contains "entity", rename
+    if (variableDec.getName().match(/entity/g) != -1) {
+      newVariableDec = variableDec.getName().replace("entity", renameTo);
       variableDec.rename(newVariableDec);
     }
   }
 
-  // rename expressions containing "entity" || "Entity"
+  // rename expressions of type SyntaxKind.ExpressionStatement containing "entity" || "Entity"
   for (const expression of sourceFile.getDescendantsOfKind(
     SyntaxKind.ExpressionStatement
   )) {
-    if (expression.getText().indexOf("Entity") > -1) {
-      newExpression = expression.getText().replace("Entity", "Premenovane");
+    if (expression.getText().match(/Entity/g) != -1) {
+      newExpression = expression.getText().replace("Entity", renameTo);
       expression.setExpression(newExpression);
     }
-    if (expression.getText().indexOf("entity") > -1) {
-      newExpression = expression.getText().replace("entity", "premenovane");
+    if (expression.getText().match(/entity/g) != -1) {
+      newExpression = expression.getText().replace("entity", renameTo);
       expression.setExpression(newExpression);
     }
   }
 
-  // rename file if contains "Entity"
-  if (sourceFile.getFilePath().indexOf("Entity") > -1) {
-    newFilePath = sourceFile
-      .getFilePath()
+  // rename string literals of type SyntaxKind.StringLiteral containing "entity" || "Entity"
+  for (const stringLiteral of sourceFile.getDescendantsOfKind(
+    SyntaxKind.StringLiteral
+  )) {
+    if (stringLiteral.getLiteralText().match(/Entity/g) != -1) {
+      newStringLiteral = stringLiteral
+        .getLiteralText()
+        .replace("Entity", renameTo);
+      stringLiteral.setLiteralValue(newStringLiteral);
+    }
+    if (stringLiteral.getLiteralText().match(/entity/g) != -1) {
+      newStringLiteral = stringLiteral
+        .getLiteralText()
+        .replace("entity", renameTo);
+      stringLiteral.setLiteralValue(newStringLiteral);
+    }
+  }
+
+  // rename file if contains "Entity" and move to final folder
+  if (sourceFile.getFilePath().match(/Entity/g) != -1) {
+    newFileName = sourceFile
+      .getBaseName()
       .toString()
-      .replace("Entity", "Premenovane");
-    sourceFile.move(newFilePath);
+      .replace("Entity", renameTo);
+    sourceFile.move("./modifiedFiles/" + newFileName);
+  } else {
+    // else just move to final folder without renaming
+    sourceFile.move("./modifiedFiles/");
   }
 }
+
+//save changes
 project.save();
--- toBeModified/EntityKomponentList.tsx
+++ toBeModified/EntityKomponentList.tsx
@@ -1,4 +1,5 @@
 export const EntityKomponent = (props) => {
   [data] = useEntityQuery()
+  const entityLiteral = "Entity string";
   return data.map(d => <EntityKomponent data={d} />
 }

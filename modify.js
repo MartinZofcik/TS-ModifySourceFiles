cd4e98d65cc0b1f8f7767a9496bf331198f3fd71
Martin Zofcik <martinzof1@gmail.com>
2023-08-26 12:06:54 +0200

    Add base; renaming variables expressions, files working; dirty if statements, no regex

--- modify.js
+++ modify.js
@@ -2,12 +2,44 @@ import { Project, SyntaxKind } from "ts-morph";
 
 const project = new Project({ tsConfigFilePath: "tsconfig.json" });
 
+var newVariableDec, newExpression, newFilePath;
+
 for (const sourceFile of project.getSourceFiles("EntityKomponentList.tsx")) {
-  for (const arrowFunc of sourceFile.getDescendantsOfKind(
+  // rename variables containing "entity" || "Entity"
+  for (const variableDec of sourceFile.getDescendantsOfKind(
     SyntaxKind.VariableDeclaration
-  ))
-    if (arrowFunc.getName() === "EntityKomponent") {
-      arrowFunc.rename("aaaa");
+  )) {
+    if (variableDec.getName().indexOf("Entity") > -1) {
+      newVariableDec = variableDec.getName().replace("Entity", "Premenovane");
+      variableDec.rename(newVariableDec);
     }
+    if (variableDec.getName().indexOf("entity") > -1) {
+      newVariableDec = variableDec.getName().replace("entity", "premenovane");
+      variableDec.rename(newVariableDec);
+    }
+  }
+
+  // rename expressions containing "entity" || "Entity"
+  for (const expression of sourceFile.getDescendantsOfKind(
+    SyntaxKind.ExpressionStatement
+  )) {
+    if (expression.getText().indexOf("Entity") > -1) {
+      newExpression = expression.getText().replace("Entity", "Premenovane");
+      expression.setExpression(newExpression);
+    }
+    if (expression.getText().indexOf("entity") > -1) {
+      newExpression = expression.getText().replace("entity", "premenovane");
+      expression.setExpression(newExpression);
+    }
+  }
+
+  // rename file if contains "Entity"
+  if (sourceFile.getFilePath().indexOf("Entity") > -1) {
+    newFilePath = sourceFile
+      .getFilePath()
+      .toString()
+      .replace("Entity", "Premenovane");
+    sourceFile.move(newFilePath);
+  }
 }
 project.save();

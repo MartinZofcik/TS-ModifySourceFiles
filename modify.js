1e11f97db95db6f14af7c48a4c9b0cef2cf3b2ca
Martin Zofcik <martinzof1@gmail.com>
2023-08-30 12:37:30 +0200

    Add separate functions to rename based on AST type

--- modify.js
+++ modify.js
@@ -3,70 +3,76 @@ import { Project, SyntaxKind } from "ts-morph";
 // create and configurate ts-morph project
 const project = new Project({ tsConfigFilePath: "tsconfig.json" });
 
-var variableMatches,
-  newVariableDec,
-  expressionMatches,
-  newExpression,
-  stringLiteralMatches,
-  newStringLiteral,
-  newFileName;
-
 // get command-line parameter
 const renameTo = process.argv.slice(2);
 
-// loop over entire folder
-for (const sourceFile of project.getSourceFiles("./toBeModified/*.tsx")) {
-  // rename variables of type SyntaxKind.VariableDeclaration containing "entity" || "Entity"
+const renameVarDeclarations = (sourceFile) => {
   for (const variableDec of sourceFile.getDescendantsOfKind(
     SyntaxKind.VariableDeclaration
   )) {
-    variableMatches = variableDec.getName().match(/Entity/gi);
+    const variableMatches = variableDec.getName().match(/Entity/gi);
     // if variable contains "Entity", rename
     if (variableMatches) {
-      newVariableDec = variableDec
+      const newVariableDec = variableDec
         .getName()
         .replace(variableMatches[0], renameTo);
       variableDec.rename(newVariableDec);
     }
   }
+};
 
-  // rename expressions of type SyntaxKind.ExpressionStatement containing "entity" || "Entity"
+const renameExpressions = (sourceFile) => {
   for (const expression of sourceFile.getDescendantsOfKind(
     SyntaxKind.ExpressionStatement
   )) {
-    expressionMatches = expression.getText().match(/Entity/gi);
+    const expressionMatches = expression.getText().match(/Entity/gi);
     if (expressionMatches) {
-      newExpression = expression
+      const newExpression = expression
         .getText()
         .replace(expressionMatches[0], renameTo);
       expression.setExpression(newExpression);
     }
   }
+};
 
-  // rename string literals of type SyntaxKind.StringLiteral containing "entity" || "Entity"
+const renameStringLiterals = (sourceFile) => {
   for (const stringLiteral of sourceFile.getDescendantsOfKind(
     SyntaxKind.StringLiteral
   )) {
-    stringLiteralMatches = stringLiteral.getLiteralText().match(/Entity/gi);
+    const stringLiteralMatches = stringLiteral
+      .getLiteralText()
+      .match(/Entity/gi);
     if (stringLiteralMatches) {
-      newStringLiteral = stringLiteral
+      const newStringLiteral = stringLiteral
         .getLiteralText()
         .replace(stringLiteralMatches[0], renameTo);
       stringLiteral.setLiteralValue(newStringLiteral);
     }
   }
+};
+
+// loop over entire folder
+for (const sourceFile of project.getSourceFiles("./toBeModified/*.tsx")) {
+  // rename variables of type SyntaxKind.VariableDeclaration containing "entity" || "Entity"
+  renameVarDeclarations(sourceFile);
+
+  // rename expressions of type SyntaxKind.ExpressionStatement containing "entity" || "Entity"
+  renameExpressions(sourceFile);
+
+  // rename string literals of type SyntaxKind.StringLiteral containing "entity" || "Entity"
+  renameStringLiterals(sourceFile);
 
   // rename file if contains "Entity" and move to final folder
-  if (sourceFile.getFilePath().match(/Entity/g) != -1) {
-    newFileName = sourceFile
-      .getBaseName()
-      .toString()
-      .replace("Entity", renameTo);
-    sourceFile.move("./modifiedFiles/" + newFileName);
-  } else {
-    // else just move to final folder without renaming
-    sourceFile.move("./modifiedFiles/");
-  }
+  // if (sourceFile.getFilePath().match(/Entity/g) != -1) {
+  //   const newFileName = sourceFile
+  //     .getBaseName()
+  //     .toString()
+  //     .replace("Entity", renameTo);
+  //   sourceFile.move("./modifiedFiles/" + newFileName);
+  // } else {
+  //   // else just move to final folder without renaming
+  //   sourceFile.move("./modifiedFiles/");
+  // }
 }
 
 //save changes
